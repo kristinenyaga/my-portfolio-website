a31ebbf15f65cdf1986e53d292d549ad3f11f791
krisine <kristine.nyaga@student.moringaschool.com>
2023-07-06 22:51:52 +0300

    hero section

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,24 +1,38 @@
-import line from "../../assets/line.svg"
+import Email from "./email"
+// import computer from "../../assets/computer.jpeg"
+import { motion } from "framer-motion"
+
+import Socials from "./socials"
+import Banner from "./banner"
 const Hero = () => {
   return (
-    <div className="flex flex-row items-center justify-between">
-      <div className="flex flex-col justify-items-start max-w-screen-xl justify-start">
-        <div className="flex flex-row gap-2">
-          <h2 className="leading-8  text-2xl">Hello </h2>
-          <img src={line} alt="line" className="" />
-        </div>
-        <p className="text-4xl ">Christine Nyaga</p>
-        <p className="text-5xl text-gray-600">Full Stack Web Developer</p>
-        <p className="max-w-screen-md">As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications.
-          Explore my latest projects and articles, showcasing my expertise in React.
-          js and web development.</p>
-        <button type='button' className='text-md text-center border p-2 px-6 rounded-md  border-teal-500 text-teal-500' onClick={() => window.open('https://github.com/kristinenyaga')}>resume</button>
-      </div>
-      <div>
-        <h1>hello</h1>
+    <div className="flex flex-row w-full h-[60vh] items-center gap-20 justify-between ">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 1.5 }}
+        className="hidden xl:inline-flex h-[80vh] w-32 fixed bottom-0">
+        <Socials />
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0,y: -30 }}
+        animate={{ opacity: 1,y:0 }}
+        transition={{
+          type: "spring",
+          stiffness: 300,
+          delay: 0.7,
+          duration: 1,
+          bounce: 2
+          
+        }}
+        className=" mx-auto gap-6">
+         <Banner />
+        </motion.div>
+
+      <div className=" fixed right-0 bottom-0 h-[30vh]">
+        <Email />
       </div>
     </div>
-
   )
 }
 
