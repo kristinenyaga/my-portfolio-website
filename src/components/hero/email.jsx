2626ef6df75947e385c139dfc5f92e5e09193399
krisine <kristine.nyaga@student.moringaschool.com>
2023-07-06 22:51:34 +0300

    rightside email

---
+++ src/components/hero/email.jsx
@@ -0,0 +1,18 @@
+import line from "../../assets/line.svg"
+
+const Email = () => {
+  return (
+    <div className="">
+      <a href="mailto:nyagakristine@gmail.com">
+        <p className="rotate-90 tracking-widest text-teal-500 gap-7 flex">
+          nyagakristine@gmail.com
+          {/* <img src={line} alt="line" className="" /> */}
+        </p>
+
+      </a>
+
+    </div>
+  )
+}
+
+export default Email
